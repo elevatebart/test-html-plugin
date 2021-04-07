e68ac46d8086624c9f68b9e9212045232d6069c4
ElevateBart <ledouxb@gmail.com>
2021-04-07 17:11:30 -0500

    first commit

---
+++ src/App.spec.js
@@ -0,0 +1,9 @@
+import { mount } from "@cypress/react";
+import App from "./App.js";
+
+describe("<App/>", () => {
+  it("should say hello", () => {
+    mount(<App name="World" />);
+    cy.contains("h1", "Hello World");
+  });
+});
